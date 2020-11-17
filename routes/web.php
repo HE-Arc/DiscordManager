ea7e2111a4fe1e486069764a0f137be53cb85b58
Antoine L <antoine.lestrade@he-arc.ch>
2020-11-17 19:41:18 +0100

    Ajout Middleware CheckConnected

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,16 @@ use \App\Http\Controllers\HomeController;
 */
 Route::redirect("/","/welcome");
 Route::view("/welcome","welcome.index")->name("welcome");
-Route::get("/home", [HomeController::class, 'index'])->name("home");
-
 Route::get('/login', [LoginController::class, 'redirectToProvider'])->name("login");
-Route::get('/add-bot/{id}',[LoginController::class, 'addBot'])->name("add-bot");
-
-//Route::redirect("/login-callback","/home");
 Route::get('/login-callback', [LoginController::class, 'loginCallback']);
 
-Route::get('/discord/bot-added', [LoginController::class, 'handleBotCallback']);
+Route::group([
+    'middleware' => 'App\Http\Middleware\CheckConnected'
+], function (){
+    Route::get("/home", [HomeController::class, 'index'])->name("home");
+    Route::get('/add-bot/{id}',[LoginController::class, 'addBot'])->name("add-bot");
+    Route::get('/discord/bot-added', [LoginController::class, 'handleBotCallback']);
+    Route::view("/register","welcome")->name("register");
+});
 
 
-//Route::view("/login","welcome")->name("login");
-Route::view("/register","welcome")->name("register");
-
-//Route::get('/yo', function () {
-//    return view('welcome');
-//});
