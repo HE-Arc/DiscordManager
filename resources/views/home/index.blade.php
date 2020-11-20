27794c909ce03d8903eb732291facf8ef104ddf0
MarieArielle <marie.beguin@he-arc.ch>
2020-11-20 11:30:34 +0100

    Update serveur list interface

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,12 +5,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm">
-                    <h1>Manage server</h1>
-                    <ul class="list-group">
+                    <h1 style="color:#ffffff;">Manage server</h1>
+                    <ul class=" list-group list-group-flush">
                         @foreach ($InGuildList as $guild)
-                            <li class="list-group-item">
-                                <h5>
-                                    <a class="" href="{{route("add-bot",$guild->id) }}">
+                            <li class="list-group-item" style="background-color:#23272A;">
+                                <h5 >
+                                    <a style="color:#7289DA;" href="{{route("add-bot",$guild->id) }}">
                                         <img
                                             @if($guild->icon != null)
                                             src="https://cdn.discordapp.com/icons/{{$guild->id}}/{{$guild->icon}}.png"
@@ -18,12 +18,12 @@
                                             src="https://cdn.discordapp.com/app-icons/761513537825669130/6436659f90801b9ac8b9a5e7dac56bfb.png"
                                             @endif
                                             alt="Image de guilde"
-                                            class="img-thumbnail rounded-circle rounded-sm border-0" style="height: 50px; ">
-
+                                            class="rounded-circle " style="height: 50px; ">
                                         {{$guild->name}}
                                         @if($guild->owner == true)
-                                            <span class="badge badge-primary">Owner</span>
+                                            <span class="badge badge-primary" style="background-color:#7289DA;">Owner</span>
                                         @endif
+                                        <a class="btn btn-primary btn-lg justify-content-right  " href="{{route("login")}}" role="button">WIP path</a>
                                     </a>
                                 </h5>
                             </li>
@@ -31,12 +31,12 @@
                     </ul>
                 </div>
                 <div class="col-sm">
-                    <h1>Add server</h1>
-                    <ul class="list-group">
+                    <h1 style="color:#ffffff;">Add server</h1>
+                    <ul class="list-group list-group-flush" >
                         @foreach ($NotInGuildList as $guild)
-                            <li class="list-group-item">
+                            <li class="list-group-item" style="background-color:#23272A;">
                                 <h5>
-                                    <a class="" href="{{route("add-bot",$guild->id) }}">
+                                    <a class="" style="color:#7289DA;" href="{{route("add-bot",$guild->id) }}">
                                         <img
                                             @if($guild->icon != null)
                                             src="https://cdn.discordapp.com/icons/{{$guild->id}}/{{$guild->icon}}.png"
@@ -44,11 +44,11 @@
                                             src="https://cdn.discordapp.com/app-icons/761513537825669130/6436659f90801b9ac8b9a5e7dac56bfb.png"
                                             @endif
                                             alt="Image de guilde"
-                                            class="img-thumbnail rounded-circle rounded-sm border-0" style="height: 50px; ">
+                                            class="rounded-circle " style="height: 50px; ">
 
                                         {{$guild->name}}
                                         @if($guild->owner == true)
-                                            <span class="badge badge-primary">Owner</span>
+                                            <span class="badge badge-primary" style="background-color:#7289DA;">Owner</span>
                                         @endif
                                     </a>
                                 </h5>
