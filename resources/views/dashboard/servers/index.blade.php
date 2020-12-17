eb64ec90ac5b75b1a39bff0adab98ee06655daf4
Marie Béguin <marie.beguin@he-arc.ch>
2020-12-17 09:14:40 +0100

    Update icone

--- resources/views/dashboard/servers/index.blade.php
+++ resources/views/dashboard/servers/index.blade.php
@@ -19,7 +19,7 @@
                                         @if($guild->icon != null)
                                         src="https://cdn.discordapp.com/icons/{{$guild->id}}/{{$guild->icon}}.png"
                                         @else
-                                        src="https://cdn.discordapp.com/app-icons/761513537825669130/6436659f90801b9ac8b9a5e7dac56bfb.png"
+                                        src="/logo.svg"
                                         @endif
                                         alt="Image de guilde"
                                         class="rounded-circle">
@@ -52,7 +52,7 @@
                                             @if($guild->icon != null)
                                             src="https://cdn.discordapp.com/icons/{{$guild->id}}/{{$guild->icon}}.png"
                                             @else
-                                            src="https://cdn.discordapp.com/app-icons/761513537825669130/6436659f90801b9ac8b9a5e7dac56bfb.png"
+                                            src="/logo.svg"
                                             @endif
                                             alt="Image de guilde"
                                             class="rounded-circle mx-2">
